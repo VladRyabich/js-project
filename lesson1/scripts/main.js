ab827842eaccaa6187eeebed20b190d700c3c323
Vlad Ryabich <107134617+VladRyabich@users.noreply.github.com>
2024-04-30 13:35:31 +0300

    - Added the files to the directory "Lesson1"

---
+++ lesson1/scripts/main.js
@@ -0,0 +1,74 @@
+// Завдання 1
+
+let welcome = 'hello';
+console.log(welcome);
+
+let wordOwu = 'owu';
+console.log(wordOwu);
+
+let wordCom = 'com';
+console.log(wordCom);
+
+let region = 'ua';
+console.log(region);
+
+let numberOne = 1;
+console.log(numberOne);
+
+let numberTen = 10;
+console.log(numberTen);
+
+let negativeNumber = -999;
+console.log(negativeNumber);
+
+let numberOneTwoThree = 123;
+console.log(numberOneTwoThree);
+
+let numberPi = 3.14;
+console.log(numberPi);
+
+let floatingNumber = 2.7;
+console.log(floatingNumber);
+
+let numberSixteen = 16;
+console.log(numberSixteen);
+
+let yes = true;
+console.log(yes);
+
+let not = false;
+console.log(not);
+
+
+// Завдання 2
+
+let firstName = 'Vladislav';
+let middleName = 'Yuriyovych';
+let lastName = 'Ryabich';
+
+let fullName = `${lastName} ${firstName} ${middleName}`;
+console.log(fullName);
+
+
+// Завдання 3
+
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+
+
+// Додаткове завдання
+
+let name = prompt('Ваше імя', 'Vladislav');
+console.log(name);
+
+let surname = prompt('По батькові', 'Yuriyovych');
+console.log(surname);
+
+let dateOfBirth = prompt('Дата народження', '05.03.1997');
+console.log(dateOfBirth);
