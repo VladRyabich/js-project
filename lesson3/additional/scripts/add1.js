a0d66b2743033f0529a1ae82d24346d0f408772c
Vlad Ryabich <107134617+VladRyabich@users.noreply.github.com>
2024-05-08 13:00:28 +0300

    - Added the files to the directory "Lesson3"

--- lesson3/additional/scripts/add1.js
+++ lesson3/additional/scripts/add1.js
@@ -1,29 +1,4 @@
 /*
-- Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
-- Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.
-- Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
-- Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
-- Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи
-- Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи
-
-- Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів. Вивести в консоль всі його елементи в циклі.
-- Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
-- Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
-- Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
-- Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
-- Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
-
-
-
-стоврити масив книжок (назва, кількість сторінок, автори , жанри).
--знайти наібльшу книжку.
-- знайти книжку/ки з найбільшою кількістю жанрів
-- знайти книжку/ки з найдовшою назвою
-- знайти книжку/ки які писали 2 автори
-- знайти книжку/ки які писав 1 автор
-*/
-
-/*
 --створити масив з:
     - з 5 числових значень
     - з 5 стічкових значень
@@ -32,7 +7,6 @@
 
     -- Створити пустий масив. Наповнити його будь-якими значеннями звертаючись до конкретного індексу. Вивести в консоль
  */
-
 let numbers = [1, 13, 27, 55, 77];
 
 let carBrands = ['BMW', 'Nissan', 'Audi', 'Mazda', 'Kia'];
@@ -66,6 +40,7 @@ let players = [
 ];
 
 console.log(numbers, carBrands, players);
+console.log('');
 
 /*
 - є масив [2,17,13,6,22,31,45,66,100,-18] :
@@ -79,7 +54,6 @@ console.log(numbers, carBrands, players);
     8. вивести масив в зворотньому порядку.
     9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
  */
-
 //1. перебрати його циклом while
 let nums = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
 
@@ -93,14 +67,12 @@ document.write(`</div>`);
 
 //2. перебрати його циклом for
 document.write(`<div>`);
-for (num of nums) {
-    document.write(`${num}`)
+for (i = 0; i < nums.length; i++) {
+    document.write(`${nums[i]}`)
 }
 document.write(`</div>`);
 
 //3. перебрати циклом while та вивести  числа тільки з непарним індексом
-// let nums2 = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
-
 document.write(`<ul>`);
 let a = 0;
 while (a < nums.length) {
@@ -115,15 +87,12 @@ document.write(`</ul>`);
 document.write(`<ul>`);
 for (a = 0; a < nums.length; a++) {
     if (a % 2 !== 0) {
-        num2 = nums[a];
-        document.write(`<li>${num2}</li>`);
+        document.write(`<li>${nums[a]}</li>`);
     }
 }
 document.write(`</ul>`);
 
 // 5. перебрати циклом while та вивести  числа тільки парні  значення
-// let nums3 = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
-
 document.write(`<ul>`);
 let b = 0;
 while (b < nums.length) {
@@ -135,7 +104,6 @@ while (b < nums.length) {
 document.write(`</ul>`);
 
 // 6. перебрати циклом for та вивести  числа тільки парні  значення
-
 document.write(`<ul>`);
 for (b = 0; b < nums.length; b++) {
     if (b % 2 === 0) {
@@ -145,15 +113,16 @@ for (b = 0; b < nums.length; b++) {
 document.write(`</ul>`);
 
 // 7. замінити кожне число кратне 3 на слово "okten"
-let for3 = [];
+document.write(`<ul>`);
 for (let num of nums) {
     if (num % 3 === 0) {
-        console.log(num);
         num = 'okten';
-        for3[for3.length] = num
+        document.write(`<li>${num}</li>`);
+    } else {
+        document.write(`<li>${num}</li>`);
     }
-    console.log(num);
 }
+document.write(`</ul>`);
 
 // 8. вивести масив в зворотньому порядку.
 document.write(`<ul>`);
@@ -207,20 +176,146 @@ for (let k = nums.length - 1; k >= 0; k--) {
 }
 document.write(`</ul>`);
 
+// - Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+let number = [57, 23, 89, 42, 6, 77, 31, 15, 98, 10];
+console.log('Масив з 10 числових елементів:');
+let v = 0;
+while (v < number.length) {
+    console.log(number[v]);
+    v++
+}
+console.log('');
+
+// - Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.
+let colors = ['Зелений', 'Голубий', 'Рожевий', 'Червоний', 'Жовтий', 'Білий', 'Фіолетовий', 'Оранжевий', 'Чорний', 'Сірий']
+console.log('Масив з 10 строкрових елементів:');
+for (s = 0; s < colors.length; s++) {
+    console.log(colors[s])
+}
+console.log('');
+
+// - Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
+let myArray = [5, "Hello", true, {name: "John", age: 30}, ["apple", "banana"], null, undefined, function () {console.log("This is a function");}, NaN, -12.345];
+console.log('Масив з 10 елементів будь-якого типу:');
+for (n = 0; n < myArray.length; n++) {
+    console.log(myArray[n]);
+}
+console.log('');
+
+// - Створити масив з 10 елементів числового, стірчкового і булевого типу.
+// За допомогою if та typeof вивести тільки булеві елементи
+let mixedArray = [5, true, "Hello", 10, false, "World", -3.14, true, "JavaScript", 42];
+console.log('За допомогою if та typeof вивести тільки булеві елементи:');
+for (m = 0; m < mixedArray.length; m++) {
+    if (typeof mixedArray[m] === 'boolean') {
+        console.log(mixedArray[m]);
+    }
+}
+console.log('');
+
+// - Створити масив з 10 елементів числового, стірчкового і булевого типу.
+// За допомогою if та typeof вивести тільки числові елементи
+console.log('За допомогою if та typeof вивести тільки числові елементи:');
+for (m = 0; m < mixedArray.length; m++) {
+    if (typeof mixedArray[m] === 'number') {
+        console.log(mixedArray[m]);
+    }
+}
+console.log('');
+
+// - Створити масив з 10 елементів числового, стрічкового і булевого типу.
+// За допомогою if та typeof вивести тільки рядкові елементи
+console.log('За допомогою if та typeof вивести тільки рядкові елементи:');
+m = 0;
+while (m < mixedArray.length) {
+    if (typeof mixedArray[m] === 'string') {
+        console.log(mixedArray[m]);
+    }
+    m++
+} // пробую різні варіанти циклів
+console.log('');
+
+// - Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів.
+// Вивести в консоль всі його елементи в циклі.
+let newArray = [];
+
+newArray[0] = 'OpenAI';
+newArray[1] = 25;
+newArray[2] = false;
+newArray[3] = '2024';
+newArray[4] = true;
+newArray[5] = 3.13159;
+newArray[6] = 'HTML';
+newArray[7] = -10;
+newArray[8] = true;
+newArray[9] = 'try';
+
+for (const newArrayElement of newArray) {
+    console.log(newArrayElement);
+}
+console.log('');
+
+// - Створити цикл for на 10  ітерацій з кроком 1.
+//Вивести поточний номер кроку через console.log та document.write
+console.log('Цикл for на 10  ітерацій з кроком 1:');
+document.write('Цикл for на 10  ітерацій з кроком 1:');
+for (l = 0; l < newArray.length; l++) {
+    console.log('Поточний номер кроку: ' + (l + 1));
+    document.write(`<p>Поточний номер кроку:</p> ${l + 1} <br>`);
+}
+document.write('<br>');
+console.log('');
+
+// - Створити цикл for на 100 ітерацій з кроком 1.
+// Вивести поточний номер кроку через console.log та document.write
+console.log('Цикл for на 100 ітерацій з кроком 1:');
+document.write('Цикл for на 100 ітерацій з кроком 1:' + '<br>');
+for (l = 0; l < 100; l++) {
+    console.log('Поточний номер кроку: ' + (l + 1));
+    document.write('Поточний номер кроку: ' + (l + 1) + '<br>');
+}
+document.write('<br>');
+console.log('');
+
+// - Створити цикл for на 100 ітерацій з кроком 2.
+// Вивести поточний номер кроку через console.log та document.write
+console.log('Цикл for на 100 ітерацій з кроком 2:');
+document.write('Цикл for на 100 ітерацій з кроком 2:' + '<br>');
+for (l = 0; l < 100; l += 2) {
+    console.log('Поточний номер кроку: ' + (l + 2));
+    document.write('Поточний номер кроку: ' + (l + 2) + '<br>');
+}
+document.write('<br>');
+console.log('');
+
+// - Створити цикл for на 100 ітерацій.
+// Вивести тільки парні кроки через console.log + document.write
+console.log('Цикл for на 100 ітерацій. Тільки парні кроки:');
+document.write('Цикл for на 100 ітерацій. Тільки парні кроки:' + '<br>');
+for (l = 0; l < 100; l++) {
+    if (l % 2 === 0) {
+        console.log('Поточний номер кроку: ' + l);
+        document.write('Поточний номер кроку: ' + l + '<br>');
+    }
+}
+document.write('<br>');
+console.log('');
+
+// - Створити цикл for на 100 ітерацій.
+// Вивести тільки непарні кроки через console.log + document.write
+console.log('Цикл for на 100 ітерацій. Тільки непарні кроки:');
+document.write('Цикл for на 100 ітерацій. Тільки непарні кроки:' + '<br>');
+for (l = 0; l < 100; l++) {
+    if (l % 2 !== 0) {
+        console.log('Поточний номер кроку: ' + l);
+        document.write('Поточний номер кроку: ' + l + '<br>');
+    }
+}
+document.write('<br>');
+console.log('');
 
 //// стоврити масив книжок (назва, кількість сторінок, автори , жанри).
-//
-// - знайти книжку/ки з найбільшою кількістю жанрів
-// - знайти книжку/ки з найдовшою назвою
-// - знайти книжку/ки які писали 2 автори
-// - знайти книжку/ки які писав 1 автор
 let books = [
-    {
-        title: "Ігри розуму",
-        pages: 320,
-        authors: ["Денніел Канеман", "Амос Тверський"],
-        genres: ["психологія", "економіка"]
-    },
     {
         title: "Майстер і Маргарита",
         pages: 384,
@@ -231,7 +326,7 @@ let books = [
         title: "Гаррі Поттер і філософський камінь",
         pages: 332,
         authors: ["Джоан Роулінг"],
-        genres: ["фентезі", "пригоди"]
+        genres: ["фентезі", "пригоди", "детектив"]
     },
     {
         title: "1984",
@@ -244,6 +339,12 @@ let books = [
         pages: 96,
         authors: ["Антуан де Сент-Екзюпері"],
         genres: ["філософська проза", "пригоди"]
+    },
+    {
+        title: "Ігри розуму",
+        pages: 320,
+        authors: ["Денніел Канеман", "Амос Тверський"],
+        genres: ["психологія", "економіка"]
     },
     {
         title: "Таємниця Шерлока Холмса",
@@ -274,18 +375,61 @@ let books = [
         pages: 272,
         authors: ["Оскар Уайльд"],
         genres: ["філософська проза", "готичний роман"]
-    },
+    }
 ];
 
 // -знайти наібльшу книжку.
-
-
-for (i = 0; i < books.length; ++i) {
-    if (books[i] > books["pages"]) {
-        continue;
+let bigBook = books[0];
+for (const book of books) {
+    if (book.pages > bigBook.pages) {
+        bigBook = book;
     }
-    if (books[i] < books["pages"]) {
-        console.log(books["pages"]);
-    }
-    console.log(books[i], i)
 }
+console.log('Найбільша книга:');
+console.log(bigBook);
+console.log('');
+
+// - знайти книжку/ки з найбільшою кількістю жанрів
+let bigGenres = books[0];
+for (const bookGenre of books) {
+    if (bookGenre.genres.length > bigGenres.genres.length) {
+        bigGenres = bookGenre;
+    }
+}
+console.log('Найбільша кількість жанрів у книги:');
+console.log(bigGenres);
+console.log('');
+
+// - знайти книжку/ки з найдовшою назвою
+maxLength = 0;
+let longestTitle;
+for (const bookTitle of books) {
+    if (bookTitle.title.length > maxLength) {
+        maxLength = bookTitle.title.length;
+        longestTitle = bookTitle;
+    }
+}
+console.log('Книга з найдовшою назвою:');
+console.log(longestTitle);
+console.log('');
+
+// - знайти книжку/ки які писали 2 автори
+let twoAuthors = books[0];
+for (const bookAuthors of books) {
+    if (bookAuthors.authors.length === 2) {
+        twoAuthors = bookAuthors;
+    }
+}
+console.log('Книги з двома авторами:');
+console.log(twoAuthors);
+console.log('');
+
+// - знайти книжку/ки які писав 1 автор
+let oneAuthor = books[0];
+for (const bookAuthor of books) {
+    if (bookAuthor.authors.length === 1) {
+        oneAuthor = bookAuthor;
+    }
+}
+console.log('Книги з одним автором:');
+console.log(oneAuthor);
